d725401f2fcca10cafba733184bb80e5379d93ca
harikesh rajbhar <harikesh.rajbhar@soprasteria.com>
2025-07-24 14:09:45 +0530

    implemented forgot and reset password

--- controllers/userControler.js
+++ controllers/userControler.js
@@ -2,6 +2,10 @@ const { userModel } = require("../models/user");
 const { generateToken } = require("../middlewares/authToken");
 const bcrypt = require("bcryptjs");
 
+const nodemailer = require('nodemailer')
+
+const Jwt = require("jsonwebtoken"); //will create seprate authtoken for forget password
+
 const handleCreateNewUser = async (req, res) => {
   let { userName, email, phoneNo, password } = req.body;
   try {
@@ -44,9 +48,6 @@ const handleLoginUser = async (req, res) => {
       return res.status(400).json({ message: "User not found" });
     }
     if (all_data.length > 0) {
-      // let findUserIndex = all_data.findIndex(
-      //   (itm) => itm.email === email && itm.password === password
-      // );
       let findUserIndex = all_data.findIndex((itm) => itm.email === email);
 
       if (findUserIndex < 0) {
@@ -110,9 +111,73 @@ const handlUserDetails = async (req, res) => {
   }
 };
 
+const handleSendOpt = async (req, res)=>{
+ const {email} = req.body
+ await userModel.findOne({email: email})
+ .then(user=>{
+  if(!user){
+     res.status(400).json({ Message: "User does not exist" });
+  }
+  const token =  Jwt.sign( {id: user._id} , 'jwt-secret-token', { expiresIn: "10m" });
+   // Send the token to the user's email
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "harikeshb900@gmail.com",
+        pass: "cduf cueg lvql lyiy",
+      },
+    });
+
+    // Email configuration
+    const mailOptions = {
+      from: "harikeshb900@gmail.com",
+      to: email,
+      subject: "Reset Password",
+      html: `<h1>Reset Your Password</h1>
+    <p>Click on the following link to reset your password:</p>
+    <a href="http://localhost:3000/reset-password/${user._id}/${token}">http://localhost:3000/reset-password/${user._id}/${token}</a>
+    <p>The link will expire in 10 minutes.</p>
+    <p>If you didn't request a password reset, please ignore this email.</p>`,
+    };
+
+     // Send the email
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
+      res.status(200).json({ message: "Password reset link sent to email." });
+    });
+ })
+}
+
+const handleResetPassword = async (req, res)=>{
+ const { id, token } = req.params;
+  const { password } = req.body;
+
+  Jwt.verify(token, "jwt-secret-token", async (err, decoded) => {
+    if (err) {
+      console.log("JWT Verify Error:", err); // log for debugging
+      return res.status(400).json({ Status: "Error with token", error: err.message });
+    } else {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const updatedUser = await userModel.findByIdAndUpdate({ _id: id }, { password: hash });
+
+        res.status(200).json({ message: "Password reset successfully", result: updatedUser });
+      } catch (error) {
+        res.status(500).json({ message: "Error resetting password", error: error.message });
+      }
+    }
+  });
+}
+
 module.exports = {
   handleCreateNewUser,
   handleLoginUser,
   handleFileUpload,
   handlUserDetails,
+  handleSendOpt,
+  handleResetPassword,
 };
